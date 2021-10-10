bd0b5d98d24561255c0df3b04c9a2fd48b1b0c3a
Kevin Huffman <huffmanks2@gmail.com>
2021-10-10 13:50:33 -0400

    second commit

--- index.js
+++ index.js
@@ -18,37 +18,24 @@ resetEl.addEventListener('click', () => {
     totalTipEl.innerHTML = '0.00'
 })
 
-// Clear
-billEl.addEventListener('focus', () => {
-    billEl.value = null
-})
-
-peopleEl.addEventListener('focus', () => {
-    peopleEl.value = null
-})
-
 // Form onchange
 formEl.addEventListener('keyup', () => {
-    const percentEl = document.querySelector('input[type="radio"]:checked')
+    let bill = billEl.value
+    let people = peopleEl.value
+    const percentsEl = Array.from(document.querySelectorAll('input[name="percent"]'))
 
-    // (customTipEl.value === null || customTipEl.value === undefined || customTipEl.value === '' ? percentEl.dataset.tip : customTipEl.value )
-    // console.log(!percentEl.dataset.tip ? customTipEl.value : percentEl.dataset.tip)
+    percentsEl.map((percentEl) => {
+        percentEl.addEventListener('change', () => {
+            const percentage = percentEl.value
+            console.log(percentage)
 
-    // console.log(customTipEl.value)
+            const perPerson = (bill * (percentage / 100)) / (!people ? 1 : people)
+            perPersonEl.innerHTML = perPerson.toFixed(2)
 
-    let percentage
-
-    if (customTipEl.value) {
-        percentage = customTipEl.value
-    } else {
-        percentage = percentEl.dataset.tip
-    }
-
-    const perPerson = (billEl.value * (percentage / 100)) / (!peopleEl.value ? 1 : peopleEl.value)
-    perPersonEl.innerHTML = perPerson.toFixed(2)
-
-    const totalTip = billEl.value * (percentage / 100)
-    totalTipEl.innerHTML = totalTip.toFixed(2)
+            const totalTip = bill * (percentage / 100)
+            totalTipEl.innerHTML = totalTip.toFixed(2)
+        })
+    })
 })
 
 customTipEl.addEventListener('click', () => {
