88a4d71fd4711fa8224e40795f2739773af84e74
Kevin Huffman <huffmanks2@gmail.com>
2021-10-16 11:30:03 -0400

    third commit

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const peopleEl = document.querySelector('#people')
 const customTipEl = document.querySelector('#custom-tip')
 const customLabelEl = document.querySelector('label[for="custom-tip"]')
 
-const perPersonEl = document.querySelector('#per-person')
-const totalTipEl = document.querySelector('#total-tip')
+const tipPerEl = document.querySelector('#tip-per')
+const tipTotalEl = document.querySelector('#tip-total')
+const billPerEl = document.querySelector('#bill-per')
+const billTotalEl = document.querySelector('#bill-total')
 
 const resetEl = document.querySelector('#reset')
 
@@ -14,31 +16,43 @@ resetEl.addEventListener('click', () => {
     billEl.value = null
     peopleEl.value = null
 
-    perPersonEl.innerHTML = '0.00'
-    totalTipEl.innerHTML = '0.00'
+    tipPerEl.innerHTML = '0.00'
+    tipTotalEl.innerHTML = '0.00'
+    billPerEl.innerHTML = '0.00'
+    billTotalEl.innerHTML = '0.00'
 })
 
 // Form onchange
-formEl.addEventListener('keyup', () => {
-    let bill = billEl.value
-    let people = peopleEl.value
+
+const getUpdate = (e) => {
     const percentsEl = Array.from(document.querySelectorAll('input[name="percent"]'))
 
     percentsEl.map((percentEl) => {
-        percentEl.addEventListener('change', () => {
-            const percentage = percentEl.value
-            console.log(percentage)
+        if (e.target !== customTipEl) {
+            customTipEl.setAttribute('type', 'radio')
+            customLabelEl.style.display = 'block'
+        } else {
+            customTipEl.setAttribute('type', 'text')
+            customLabelEl.style.display = 'none'
+        }
 
-            const perPerson = (bill * (percentage / 100)) / (!people ? 1 : people)
-            perPersonEl.innerHTML = perPerson.toFixed(2)
+        if (e.target === percentEl) {
+            let percentage = e.target.value
+            // let percentage = e.target === percentEl && isNaN(e.target.value) ? percentEl.value = 0: percentEl.value
 
-            const totalTip = bill * (percentage / 100)
-            totalTipEl.innerHTML = totalTip.toFixed(2)
-        })
+            const tipPer = (billEl.value * (percentage / 100)) / (!peopleEl.value ? (peopleEl.value = 1) : peopleEl.value)
+            tipPerEl.innerHTML = tipPer.toFixed(2)
+
+            const tipTotal = billEl.value * (percentage / 100)
+            tipTotalEl.innerHTML = tipTotal.toFixed(2)
+
+            const billTotal = (+billEl.value + tipTotal).toFixed(2)
+            billTotalEl.innerHTML = billTotal
+
+            const billPer = billTotal / (!peopleEl.value ? (peopleEl.value = 1) : peopleEl.value)
+            billPerEl.innerHTML = billPer.toFixed(2)
+        }
     })
-})
+}
 
-customTipEl.addEventListener('click', () => {
-    customTipEl.setAttribute('type', 'text')
-    customLabelEl.style.display = 'none'
-})
+formEl.addEventListener('change', getUpdate)
